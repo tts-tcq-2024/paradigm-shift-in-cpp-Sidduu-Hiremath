3a9516bd4f2cb31959db28fa0016e674ab97d983
Sidduu-Hiremath <104383952+Sidduu-Hiremath@users.noreply.github.com>
2024-09-11 21:19:20 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,35 +1,60 @@
 #include <assert.h>
 #include <iostream>
+#include <string>
 
-bool CheckTemperatureIsOk(float temperature){
-  if(temperature < 0 || temperature > 45){
-    std::cout<< "Temperature is out of range!\n";
-    return false;
-  }
-  return true;
+enum BreachType {
+    NORMAL,
+    TOO_LOW,
+    TOO_HIGH
+};
+
+BreachType checkBatteryParametersAreOk(float value, float minValue, float maxValue) {
+    if (value < minValue) {
+        return TOO_LOW;
+    }
+    if (value > maxValue) {
+        return TOO_HIGH;
+    }
+    return NORMAL;
 }
 
-bool CheckSocIsOk(float soc){
-  if(soc < 20 || soc > 80){
-    std::cout<< "State of Charge out of range!\n";
-    return false;
-  }
-  return true;
+std::string getBreachMessage(const std::string& parameter, BreachType breach) {
+    switch (breach) {
+        case TOO_LOW:
+            return parameter + " is too low!\n";
+        case TOO_HIGH:
+            return parameter + " is too high!\n";
+        default:
+            return parameter + " is normal.\n";
+    }
 }
 
-bool CheckChargeRateIsOk(float ChargeRate){
-  if(ChargeRate > 0.8){
-    std::cout<< "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
+BreachType IsBatteryTemperatureOk(float temperature) {
+    return checkBatteryParametersAreOk(temperature, 0, 45);
 }
 
-bool batteryIsOk(float temperature, float soc, float chargeRate){
-  return CheckTemperatureIsOk(temperature) && CheckSocIsOk(soc) && CheckChargeRateIsOk(chargeRate);
+BreachType IsBatterySocOk(float soc) {
+    return checkBatteryParametersAreOk(soc, 20, 80);
+}
+
+BreachType IsBatteryChargeRateOk(float chargeRate) {
+    return checkBatteryParametersAreOk(chargeRate, 0, 0.8);
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate) {
+    BreachType temperatureStatus = IsBatteryTemperatureOk(temperature);
+    BreachType socStatus = IsBatterySocOk(soc);
+    BreachType chargeRateStatus = IsBatteryChargeRateOk(chargeRate);
+
+    std::cout << getBreachMessage("Temperature", temperatureStatus);
+    std::cout << getBreachMessage("State of Charge", socStatus);
+    std::cout << getBreachMessage("Charge Rate", chargeRateStatus);
+
+    return (temperatureStatus == NORMAL && socStatus == NORMAL && chargeRateStatus == NORMAL);
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+    assert(batteryIsOk(25, 70, 0.7) == true);
+    assert(batteryIsOk(50, 85, 0) == false);
+    return 0;
 }
